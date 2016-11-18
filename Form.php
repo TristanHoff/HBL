8be23d8b9a6d930b7825a32053f92562ffc62b36
TristanHoff <tristanhoff@hotmail.com>
2016-11-18 12:29:29 +0100

    Laatste
    Laatste van mij

--- Form.php
+++ Form.php
@@ -68,56 +68,56 @@
         <form>
             <div class="form-group">
                 <label for="title">Voornaam ouder 1</label>
-                <input type="text" class="form-control" placeholder="Vul hier in" name="first_name_v"  value="">
+                <input type="text" class="form-control"  name="first_name_v"  value="">
                 </div><div class="form-group">
-                Achternaam ouder 1
-                <input type="text" class="form-control" placeholder="Vul hier in" name="last_name_v"   value="">
+                <label for="title">Achternaam ouder 1</label>
+                <input type="text" class="form-control"  name="last_name_v"   value="">
                 </div><div class="form-group">
-                Voornaam ouder 2
-                <input type="text" class="form-control" placeholder="Vul hier in" name="first_name_m"  value="">
+                <label for="title">Voornaam ouder 2</label>
+                <input type="text" class="form-control"  name="first_name_m"  value="">
                 </div><div class="form-group">
-                Achternaam ouder 2</td>
-                <input type="text" class="form-control" placeholder="Vul hier in" name="last_name_m"   value="">
+                <label for="title">Achternaam ouder 2</label>
+                <input type="text" class="form-control"  name="last_name_m"   value="">
                 </div><div class="form-group">
-                Email 1
-                <input type="text" class="form-control" placeholder="Vul hier in" name="email_1"       value="">
+                <label for="title">Email 1</label>
+                <input type="text" class="form-control"  name="email_1"       value="">
                 </div><div class="form-group">
-                Email 2
-                <input type="text" class="form-control" placeholder="Vul hier in" name="email_2"       value="">
+                <label for="title">Email 2</label>
+                <input type="text" class="form-control"  name="email_2"       value="">
                 </div><div class="form-group">
-                Email kind
-                <input type="text" class="form-control" placeholder="Vul hier in" name="email_k"       value="">
+                <label for="title">Email kind</label>
+                <input type="text" class="form-control"  name="email_k"       value="">
                 </div><div class="form-group">
-                Telefoon 1
-                <input type="text" class="form-control" placeholder="Vul hier in" name="telefoon_1"  value="">
+                <label for="title">Telefoon 1</label>
+                <input type="text" class="form-control"  name="telefoon_1"  value="">
                 </div><div class="form-group">
-                Telefoon 2
-                <input type="text" class="form-control" placeholder="Vul hier in" name="telefoon_2"  value="">
+                <label for="title">Telefoon 2</label>
+                <input type="text" class="form-control"  name="telefoon_2"  value="">
                 </div><div class="form-group">
-                Telefoon 3
-                <input type="text" class="form-control" placeholder="Vul hier in" name="telefoon_k"  value="">
+                <label for="title">Telefoon 3</label>
+                <input type="text" class="form-control"  name="telefoon_k"  value="">
                 </div><div class="form-group">
-                Voornaam kind
-                <input type="text" class="form-control" placeholder="Vul hier in" name="first_name_k"  value="">
+                <label for="title">Voornaam kind</label>
+                <input type="text" class="form-control"  name="first_name_k"  value="">
                 </div><div class="form-group">
-                Achternaam kind
-                <input type="text" class="form-control" placeholder="Vul hier in" name="last_name_k"   value="">
+                <label for="title">Achternaam kind</label>
+                <input type="text" class="form-control"  name="last_name_k"   value="">
                 </div><div class="form-group">
-                Geboortedatum
-                <input type="text" class="form-control" placeholder="Vul hier in" name="birth_date"    value="">
+                <label for="title">Geboortedatum</label>
+                <input type="text" class="form-control"  name="birth_date"    value="">
                 </div><div class="form-group">
-                School
-                <input type="text" class="form-control" placeholder="Vul hier in" name="school"        value="">
+                <label for="title">School</label>
+                <input type="text" class="form-control"  name="school"        value="">
                 </div><div class="form-group">
-                Vak(ken)
-                <input type="text" class="form-control" placeholder="Vul hier in" name="vak"           value="">
+                <label for="title">Vak(ken)</label>
+                <input type="text" class="form-control"  name="vak"           value="">
                 </div><div class="form-group">
-                Dyslexie
+                <label for="title">Dyslexie</label>
                 <input type="radio" name="dyslexie"     value="ja"> Ja
                 <input type="radio" name="dyslexie"     value="nee"> Nee
                 </div><div class="form-group">
-                Extra opmerkingen</td>
-                <textarea rows="5" class="form-control" placeholder="Vul hier in" cols="22" name="opmerking"></textarea>
+                <label for="title">Extra opmerkingen</label>
+                <textarea rows="5" class="form-control"  cols="22" name="opmerking"></textarea>
                 </div><div class="form-group">
                 <input type="button" class="btn btn-primary" name="opslaan" value="Opslaan">
             </div>
